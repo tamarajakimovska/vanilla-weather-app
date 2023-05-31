64304142845c6d4a39515a0f571a60688208b15e
tamarajakimovska <tamarajakimovska007@gmail.com>
2023-05-31 13:19:10 +0200

    Updated API

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
-let apiKey = "b66a983444a5fc762f7f21239b661e19";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+function displayTemperature(response) {
+  console.log(response.data);
+}
 
-console.log(apiUrl);
+let apiKey = "b66a983444a5fc762f7f21239b661e19";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
