3b79248786d037d2087390d7149dd84f351a2464
tamarajakimovska <tamarajakimovska007@gmail.com>
2023-05-31 13:50:38 +0200

    Updated speed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "b66a983444a5fc762f7f21239b661e19";
