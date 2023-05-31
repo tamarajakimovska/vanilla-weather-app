075176bf6562f414ed332dd252e9699fb3da62b5
tamarajakimovska <tamarajakimovska007@gmail.com>
2023-05-31 14:30:26 +0200

    Updated city

--- src/app.js
+++ src/app.js
@@ -38,6 +38,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "b66a983444a5fc762f7f21239b661e19";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
